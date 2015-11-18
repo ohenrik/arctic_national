eb0cc5043dc858b3301566eaa99de5dc33e4c0ad
Ole Henrik Skogstrøm <oleskogstrom@gmail.com>
2015-11-18 01:22:15 +0100

    Split posts insto news and events

--- app/admin/news.rb
+++ app/admin/news.rb
@@ -1,9 +1,9 @@
-ActiveAdmin.register Post do
+ActiveAdmin.register Post, as: 'News' do
 
   form do |f|
     f.inputs do
       f.input :title
-
+      f.input :post_type, as: :hidden, input_html: { value: 'news' }
       has_many :post_items, sortable: :position, allow_destroy: true do |ff|
         ff.input :image , as: :file, hint: (ff.object.image.present? ? image_tag(ff.object.image.url(:admin_thumb)) : "")
         ff.input :title
@@ -15,11 +15,17 @@ ActiveAdmin.register Post do
 
   index do
     column :title do |post|
-      link_to post.title, edit_admin_post_path(post)
+      link_to post.title, edit_admin_news_path(post)
     end
     actions
   end
 
   config.filters = false
+
+  controller do
+    def scoped_collection
+      Post.where(post_type: 'news')
+    end
+  end
 
 end
